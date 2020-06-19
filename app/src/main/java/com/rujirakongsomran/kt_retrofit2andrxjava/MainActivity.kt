6a0886badb9f90efca9791c589706ad18b19815c
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-19 16:16:06 +0700

    Call API and display data

--- app/src/main/java/com/rujirakongsomran/kt_retrofit2andrxjava/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_retrofit2andrxjava/MainActivity.kt
@@ -2,10 +2,45 @@ package com.rujirakongsomran.kt_retrofit2andrxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rujirakongsomran.kt_retrofit2andrxjava.Adapter.PostAdapter
+import com.rujirakongsomran.kt_retrofit2andrxjava.Model.Post
+import com.rujirakongsomran.kt_retrofit2andrxjava.Retrofit.IMyAPI
+import com.rujirakongsomran.kt_retrofit2andrxjava.Retrofit.RetrofitClient
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var jsonApi: IMyAPI
+    internal lateinit var compositeDisposable: CompositeDisposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Init API
+        val retrofit = RetrofitClient.instance
+        jsonApi = retrofit.create(IMyAPI::class.java)
+
+        // View
+        recyclerViewPosts.setHasFixedSize(true)
+        recyclerViewPosts.layoutManager = LinearLayoutManager(this)
+        fetchData()
+    }
+
+    private fun fetchData() {
+        compositeDisposable.add(jsonApi.posts
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { posts -> displayData(posts) }
+        )
+    }
+
+    private fun displayData(posts: List<Post>?) {
+        var adapter = PostAdapter(this, posts!!)
+        recyclerViewPosts.adapter = adapter
     }
 }
